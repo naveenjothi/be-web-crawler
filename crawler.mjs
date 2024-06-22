281fd7e927a0d7a4fbdefe8ed82f7615a0abf88d
NaveenjothiP <36544639+naveenjothi@users.noreply.github.com>
2024-06-22 11:21:08 +0530

    Added ES6 features

--- crawler.mjs
+++ crawler.mjs
@@ -1,5 +1,5 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
+import axios from "axios";
+import * as cheerio from "cheerio";
 
 export const fetchHTML = async (url) => {
   try {
