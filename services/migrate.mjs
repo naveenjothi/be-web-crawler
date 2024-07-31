2b39edbebfafdc650a9d103a9ef37fb44e1ca19f
NaveenjothiP <36544639+naveenjothi@users.noreply.github.com>
2024-07-31 23:19:07 +0530

    updates

--- services/migrate.mjs
+++ services/migrate.mjs
@@ -1,6 +1,14 @@
 import axios from "axios";
 import dotenv from "dotenv";
 
+const sleep = async (ms) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(true);
+    }, ms);
+  });
+};
+
 dotenv.config();
 
 const serverURL = `http://localhost:${process.env.APP_PORT}`;
@@ -10,6 +18,9 @@ const bootstrap = async () => {
   const allLinks = resp?.data?.links || [];
   const failedLinks = [];
   for (let index = 0; index < allLinks.length; index++) {
+    if ((index + 1) % 50 == 0) {
+      await sleep(2000);
+    }
     const link = allLinks[index];
     const crawledResp = await axios.get(`${serverURL}/crawl?url=${link}`);
     try {
@@ -17,7 +28,7 @@ const bootstrap = async () => {
       const payload = crawledResp.data.data;
       await axios.post(`${serverURL}/clients`, { input: payload });
     } catch (error) {
-      console.error(error);
+      console.error("Error migrating", error?.message);
       failedLinks.push(link);
     }
   }
